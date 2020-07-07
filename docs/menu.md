25b33fd5d25c92995d08ee061de8a7ea71dd3a86
codefool0307 <shrlove888@163.com>
2020-07-07 17:08:33 +0800

    笔记

--- docs/menu.md
+++ docs/menu.md
@@ -2,7 +2,7 @@
  * @Author: 孙浩然
  * @Date: 2020-04-15 22:09:18
  * @LastEditors: 孙浩然
- * @LastEditTime: 2020-06-30 11:57:51
+ * @LastEditTime: 2020-07-07 11:15:27
  * @FilePath: \Java-Point\docs\menu.md
  * @博客地址: 个人博客，如果各位客官觉得不错，请点个赞，谢谢。[地址](https://codefool0307.github.io/JavaScholar/#/)
 --> 
@@ -12,6 +12,8 @@
 
 &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;认真学习，共同进步
 </h3>
+
+如何看源码：
 </marquee>
 
 
---
+++ yuanma/yuanmaan/src/yaunama.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class yaunama {
+    public static void main(String[] args) {
+        Boolean aTrue = Boolean.TRUE;
+        Boolean aBoolean = new Boolean(true);
+
+        Boolean aBoolean1 = Boolean.valueOf(aBoolean);
+        System.out.println(aBoolean1);
+
+        String s = Boolean.toString(true);
+        System.out.println(s);
+
+        String s1="false";
+        System.setProperty(s1, "false");
+        boolean aBoolean2 = Boolean.getBoolean(s1);
+        System.out.println(aBoolean2);
+    }
+}
